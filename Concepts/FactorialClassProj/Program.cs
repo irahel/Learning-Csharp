74ec45134a8c3ab471dc70a44b09117139d7911a
irahel <isaac_rahel@live.com>
2022-03-13 11:18:47 -0300

    concepts and factorial

---
+++ Concepts/FactorialClassProj/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class FactorialClass
+{
+    public static long Factorial(int n)
+    {
+        if ((n < 0) || (n > 20)) return -1;
+
+        long temp = n;
+        for(int i = n-1; i > 0; i--)  temp *= i;        
+        return temp;
+    }
+}
+
+class MainClass
+{
+    static int Main(string[] args)
+    {
+        if (args.Length == 0) return -1;
+        int num;      
+        if(int.TryParse(args[0], out num))
+        {
+            long result = FactorialClass.Factorial(num);
+            var final = result == -1 ? "Valor Invalido" : $"{args[0]}! = {result.ToString()}";
+            Console.WriteLine(final);
+            return 1;
+        }
+        
+        return -1;            
+    }
+}

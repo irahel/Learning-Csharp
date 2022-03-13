c109e68c830c6552814484dc831304e5d8d9ee54
irahel <isaac_rahel@live.com>
2022-03-13 08:57:15 -0300

    basics

---
+++ Basics/Variables.cs
@@ -0,0 +1,18 @@
+//Declarated types
+string myName = "Rahel";
+int age = 24;
+Console.WriteLine(myName);
+
+//String Interpolation
+Console.WriteLine($"My name is {myName} and age is {age}");
+
+//Strings is a strong type
+Console.WriteLine(myName.Length);
+
+Console.WriteLine(myName.Replace("R", "B"));
+Console.WriteLine(myName.ToUpper());
+Console.WriteLine(myName.ToLower());
+Console.WriteLine(myName.Contains("a"));
+Console.WriteLine(myName.Contains("r"));
+Console.WriteLine(myName.StartsWith("Ba"));
+Console.WriteLine(myName.EndsWith("hel"));

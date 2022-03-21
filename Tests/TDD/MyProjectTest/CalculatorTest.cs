5f3896a35f4db0ef359cc288c65137f6eed3669a
irahel <isaac_rahel@live.com>
2022-03-21 16:34:38 -0300

    complement tests

--- Tests/TDD/MyProjectTest/CalculatorTest.cs
+++ Tests/TDD/MyProjectTest/CalculatorTest.cs
@@ -1,10 +1,12 @@
-using System;
+using System.Collections.Generic;
+using System.Collections;
 using Xunit;
 
 namespace Calculator.Tests
 {
     public class CalculatorTest
     {   
+
         //Assert.True
         //Assert.False
         //Assert.Equal        
@@ -15,11 +17,21 @@ namespace Calculator.Tests
             Assert.Equal(30, Calculator.Add(15, 15));
             Assert.Equal(40, Calculator.Add(30, 10));
         }
+
+        [Theory]
+        [InlineData(10,10,20)]
+        [InlineData(15,15,30)]
+        [InlineData(20,20,40)]
+        public void TestAddTheory(int n1, int n2, int expected)
+        {
+            Assert.Equal(expected, Calculator.Add(n1, n2));
+        }
         [Fact(Skip = "Not Implemented Yet")]
         public void TestSubtract()
         {
             Assert.Equal(0, Calculator.Subtract(10,10));
-        }
+        }        
+
         [Fact(DisplayName = "Testing Division")]
         public void TestDivide()
         {

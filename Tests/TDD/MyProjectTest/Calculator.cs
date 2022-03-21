392e57878c3a212721bd9a7a1af73df331039027
irahel <isaac_rahel@live.com>
2022-03-21 17:16:24 -0300

    test quality

--- Tests/TDD/MyProjectTest/Calculator.cs
+++ Tests/TDD/MyProjectTest/Calculator.cs
@@ -7,7 +7,6 @@ namespace Calculator
         public static int Add(int number1, int number2) => number1 + number2;
         public static int Subtract(int number1, int number2) => number1 - number2;
         public static int Divide(int number1, int number2) => number1 / number2;
-        public static int Multiply(int number1, int number2) => number1 * number2;
-       
+        public static int Multiply(int number1, int number2) => number1 * number2;       
     }
 }
